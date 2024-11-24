0cb107615bb5046453862214d9557a69a77d7858
Elaina <1463967532@qq.com>
2024-11-24 17:00:34 +0800

    refactor(motor)化简了电机逻辑，增加注释

--- MDK-ARM/Hardware/motor.cpp
+++ MDK-ARM/Hardware/motor.cpp
@@ -10,6 +10,13 @@
  */
 #include "motor.h"
 #if USE_CAN_Motor
+/**
+ * @brief 发送can报文
+ * 
+ * @param data 缓存区
+ * @param len CAN数据长度
+ * @param id CANID
+ */
 void MotorCanBase_t::CanSend(uint8_t *data, uint8_t len, uint32_t id)
 {
     CAN_TxHeaderTypeDef Can_Tx;
@@ -27,16 +34,27 @@ void MotorCanBase_t::CanSend(uint8_t *data, uint8_t len, uint32_t id)
     HAL_CAN_AddTxMessage(_can, &Can_Tx, data, &TxMailbox);
 }
 #if USE_3508
+/**
+ * @brief 设置目标速度
+ * 
+ * @param target 目标速度,为线速度
+ */
 void Motor3508_t::set_speed_target(float target)
 {
     _vel_target = target * rev_fator * forward;
     pid.target_update(_vel_target);
 }
+/**
+ * @brief 电机更新,需要在循环中调用,并在其他地方更新_vel_raw
+ * 
+ */
 void Motor3508_t::update()
 {
+    //获得控制量
     int16_t control = pid.update(_vel_raw.data_int);
     _common_buffer[(_id - 1) % 4 * 2] = (control >> 8) & 0xFF;
     _common_buffer[(_id - 1) % 4 * 2] = (control) & 0xFF;
+    //发送can报文
     if (have_tx_permission)
     {
         uint32_t Canid = 0x200;
@@ -49,6 +67,12 @@ void Motor3508_t::update()
 }
 #endif
 #if USE_SteeringWheelModel
+/**
+ * @brief 归一化角度到-PI~PI
+ * 
+ * @param angle 被归一化前的角度
+ * @return float 归一化后的角度
+ */
 float MotorModule_t::normalize_angle(float angle)
 {
     while (angle > PI)
@@ -61,6 +85,13 @@ float MotorModule_t::normalize_angle(float angle)
     }
     return angle;
 }
+/**
+ * @brief 设置目标
+ * 
+ * @param vel_target 舵轮目标速度 
+ * @param angle_target 舵轮目标角度
+ * @param use_youhua 是否使用大角度优化,默认使用
+ */
 void MotorModule_t::set_target(float vel_target, float angle_target, bool use_youhua)
 {
     _vel_target = vel_target;
@@ -68,9 +99,10 @@ void MotorModule_t::set_target(float vel_target, float angle_target, bool use_yo
 
     if (use_youhua)
     {
-        // 算出轮子不反向角度与轮子反向角度
+        // 算出轮子不反向角度与轮子反向角度,反向相当于多转180度
         float delta_theta_forward = normalize_angle(_angle_target - angle_last);
         float delta_theta_reverse = normalize_angle(normalize_angle(_angle_target + PI) - angle_last);
+        //选取最小角度
         if (fabs(delta_theta_forward) > fabs(delta_theta_reverse))
         {
             _vel_target = -_vel_target;
@@ -82,10 +114,10 @@ void MotorModule_t::set_target(float vel_target, float angle_target, bool use_yo
             _angle_target = normalize_angle(_angle_target);
         }
     }
-
-    vel_int = _vel_target * vel_factor * forward;
-    angle_int = _angle_target * angle_factor;
-
+    //设置控制量
+    int16_t vel_int = _vel_target * vel_factor * forward;
+    int16_t angle_int = _angle_target * angle_factor;
+    //发送控制量
     uint8_t data[8];
     data[0] = ((int16_t)angle_int) >> 8;
     data[1] = (int16_t)angle_int;
--- MDK-ARM/Hardware/motor.h
+++ MDK-ARM/Hardware/motor.h
@@ -16,7 +16,10 @@
 #define USE_Debug 1              // 使用调试
 #include "common.h"
 #include "pid_template.h"
-
+    /**
+     * @brief 电机的原始数据联合体
+     * 
+     */
     union data_t
     {
         uint8_t data_raw[4];
@@ -25,25 +28,37 @@
         // int16_t data_int16;
     };
 
-    enum MotorType_t
-    {
-        Steering_Motor,
-        Drive_Motor,
-        Motor3508,
-        Motor2006
-
-    };
-    /*接口*/
+    /**
+     * @brief 电机接口类
+     * 
+     */
     class IMotorSpeed_t
     {
     public:
+        /**
+         * @brief 设置目标线速度
+         * 
+         * @param target 
+         */
         virtual void set_speed_target(float target) = 0;
+        /**
+         * @brief 电机更新
+         * 
+         */
         virtual void update() = 0;
+        /**
+         * @brief 设置目标角度
+         * 
+         * @param target 
+         */
         virtual void set_angle_target(float target)
         {
         }
     };
-
+    /**
+     * @brief 电机基础类
+     * 
+     */
     class MotorBase_t
     {
     public:
@@ -53,23 +68,21 @@
         MotorBase_t(uint8_t id) : _id(id)
         {
         }
-        uint8_t _id;
-        float _vel_target;
-        float _angle_target;
-        // int16_t _vel_raw;   // 转速原始数据
-        // int16_t _angle_raw; // 角度原始数据
-        data_t _vel_raw;
-        data_t _angle_raw;
-        // data_t _vel_target_union;
-        // data_t _angle_target_union;
-        float vel_target;
-        float angle_target;
+        uint8_t _id=0;        
+        float _vel_target=0;      //目标线速度
+        float _angle_target=0;    //目标角度
+        data_t _vel_raw; // 转速原始数据
+        data_t _angle_raw; // 角度原始数据
+        float _radium=0;     //电机半径
 #if USE_Debug
-        float debug; // 给调试用的
+        float debug=0; // 给调试用的
 #endif
     };
 #if USE_CAN_Motor
-    // 接口类
+    /**
+     * @brief CAN的基础封装类,包含发送can的函数与绑定can的函数
+     * 
+     */
     class MotorCanBase_t : public MotorBase_t
     {
     public:
@@ -93,9 +106,19 @@
         // uint8_t id;
     };
 #if USE_3508
+    /**
+     * @brief c620电调电机类,默认数据适配3508
+     * 
+     */
     class Motor3508_t : public MotorCanBase_t, public IMotorSpeed_t
     {
     public:
+        /**
+         * @brief Construct a new Motor3508_t object
+         * 
+         * @param id 电机的id
+         * @param have_tx_permission //电机的发送权限 
+         */
         Motor3508_t(uint8_t id, bool have_tx_permission = false) : MotorCanBase_t()
         {
             _id = id;
@@ -108,7 +131,7 @@
     private:
         uint8_t _common_buffer[8];
 
-        float rev_fator = 33; // 转速系数 从原始数据转化到真实数据的倒数
+        float rev_fator = 33; // 轴转速转速系数,真实数据=原始数据*rev_fator
         bool have_tx_permission = false;
 
         pid_base_template_t<int16_t, float> pid = pid_base_template_t<int16_t, float>({5, 2, 0, -5000, 5000, 2000});
@@ -116,7 +139,10 @@
 
 #endif
 #if USE_SteeringWheelModel
-    /*封装成模块的舵轮分块*/
+    /**
+     * @brief 封装成模块的电机类,适配2024年电机主控板
+     * 
+     */
     class MotorModule_t : public MotorCanBase_t
     {
     public:
@@ -127,16 +153,12 @@
         {
             // _type = Steering_Motor;
         }
-
-        // void set_target(int16_t vel_target, int16_t angle_target);
         void set_target(float vel_target, float angle_target, bool use_youhua = false);
         static float normalize_angle(float angle);
 
     private:
-        float angle_last;
+        float angle_last;  //上次的距离,给舵轮优化用
         int8_t forward = 1;
-        int16_t vel_int;
-        int16_t angle_int;
         int16_t angle_factor = 2608; // 45.51/PI*180
         int16_t vel_factor = 2000;
         float angle_zero = PI / 2;
